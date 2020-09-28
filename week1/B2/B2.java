c83f8babd5e2c7a0959ce9387d1b29dd2b6d2394
tthandb <tthandb@hotmail.com>
2020-09-29 00:47:34 +0700

    binh

--- week1/B2/B2.java
+++ week1/B2/B2.java
@@ -1,5 +1,15 @@
+package B2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class B2 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("week1/B2/input.txt"));
+        while (scanner.hasNext()) {
+            String mes = scanner.next("\\w+");
+            System.out.println(mes);
+        }
     }
 }
