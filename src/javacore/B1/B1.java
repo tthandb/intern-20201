db6c791a045cb70e0d460ed1a63e3615570fde58
tthandb <tthandb@hotmail.com>
2020-10-01 08:09:25 +0700

    sadasdas

--- src/javacore/B1/B1.java
+++ src/javacore/B1/B1.java
@@ -2,15 +2,25 @@ package javacore.B1;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class B1 {
     public static void main(String[] args) throws Exception {
         FileWriter writer = new FileWriter("src/javacore/test.txt");
         BufferedWriter buffer = new BufferedWriter(writer);
-        buffer.write("Welcome to java.");
-        buffer.write("Welcome to java.");
-        buffer.write("Welcome to java.");
         buffer.close();
-        System.out.println("Success...");
+        Integer[] list1 = {1, 2,  4, 5};
+        Integer[] list2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(list1));
+        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(list2));
+        HashSet<Integer> unionSet = (HashSet<Integer>) set1.clone();
+        HashSet<Integer> intersectionSet = (HashSet<Integer>) set1.clone();
+        unionSet.addAll(set2);
+        intersectionSet.retainAll(set2);
+        System.out.println("set1: " + set1.toString());
+        System.out.println("set2: " + set2.toString());
+        System.out.println("unionSet: " + unionSet.toString());
+        System.out.println("intersectionSet: " + intersectionSet.toString());
     }
 }
