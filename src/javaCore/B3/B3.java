7e0609a5aa77976414fc936f449a8ff44fcf6524
tthandb <tthandb@hotmail.com>
2020-10-09 07:04:28 +0700

    saasaf

--- src/javaCore/B3/B3.java
+++ src/javaCore/B3/B3.java
@@ -1,20 +1,15 @@
 package javaCore.B3;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.zip.CheckedOutputStream;
 
-public class B3 {
+public class fake {
     public static HashMap<String, Integer> globalFreq = new HashMap<String, Integer>();
 
     public static class CountWord implements Runnable {
-        String name;
         Integer id;
 
         public CountWord(Integer id) {
@@ -23,12 +18,11 @@ public class B3 {
 
         @Override
         public void run() {
+            System.out.println(id + " run");
             File input = new File("src/javaCore/B3/input/" + this.id + ".txt");
             ArrayList<String> wordList = new ArrayList<String>();
-//            HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
             try {
                 BufferedReader bufferedReader = new BufferedReader(new FileReader(input));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/javaCore/B3/output.txt"));
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     wordList.addAll(Arrays.asList(line.split("\\W+")));
@@ -41,27 +35,48 @@ public class B3 {
                     globalFreq.put(key, ++freq);
                 }
                 bufferedReader.close();
-                for (Map.Entry<String, Integer> result : globalFreq.entrySet()) {
-                    bufferedWriter.write(result.getKey() + " " + result.getValue() + "\n");
-                    System.out.println(result.getKey() + " " + result.getValue());
-                }
-                bufferedWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public static synchronized void blabla(){
+    public static void main(String[] args) throws IOException {
         ExecutorService es = Executors.newFixedThreadPool(6);
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
         for (int i = 0; i < 21; ++i) {
             CountWord task = new CountWord(i);
-            es.submit(task);
+            futureList.add(CompletableFuture.runAsync(task, es));
         }
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]))
+                .whenComplete((input, exception) -> {
+                    HashMap<String, Integer> sortedWordList = sortByValue(globalFreq);
+                    int cnt = 0;
+                    try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/javaCore/B3/output.txt"))) {
+                        for (Map.Entry<String, Integer> result : sortedWordList.entrySet()) {
+                            cnt++;
+                            if (cnt == 10) break;
+                            bufferedWriter.write(result.getKey() + " " + result.getValue() + "\n");
+//                            System.out.println(result.getKey() + " " + result.getValue());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Done");
+                });
         es.shutdown();
     }
-    public static void main(String[] args) {
-        blabla();
-        System.out.println("end");
+
+    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm) {
+        List<Map.Entry<String, Integer>> list =
+                new ArrayList<>(hm.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Collections.reverse(list);
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
     }
 }
+
